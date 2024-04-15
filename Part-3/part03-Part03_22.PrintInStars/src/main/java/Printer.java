062f0cbce57ef63e50f7b7e610636e70596cb1bb
galyfilsaime <filsaimegaly@hotmail.com>
2024-04-15 15:41:11 -0400

    Custom method which prints a row of stars for each number in the array

---
+++ Part-3/part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -0,0 +1,25 @@
+
+public class Printer {
+
+    public static void main(String[] args) {
+        // You can test the method here
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+
+    public static void printArrayInStars(int[] array) {
+        // Write some code in here
+        int index = 0;
+        while (index < array.length) {
+            int number = array[index];
+            int i = 0;
+            while (i < number) {
+                System.out.print("*");
+                i++;
+            }
+            System.out.println("");
+            index++;
+        }
+    }
+
+}
