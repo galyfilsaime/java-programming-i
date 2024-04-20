8c929008da7e788f539b9612cf834f84540465df
galyfilsaime <filsaimegaly@hotmail.com>
2024-04-20 14:09:38 -0400

    The program creates an account with a balance of 100.0, deposits 20.0 in it, and finally prints the balance

---
+++ Part-4/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account janeAccount = new Account("Jane's account", 100.00);
+        janeAccount.deposit(20.00);
+        System.out.println(janeAccount);
+    }
+}
